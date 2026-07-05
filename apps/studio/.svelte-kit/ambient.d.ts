7e5a01f1f1b57b9cf8cb71fa4a01174f905c9e78
Stephen Spittal <stevejtspittal@gmail.com>
2026-07-05 13:21:06 +0100

    Add generated record workbench

--- apps/studio/.svelte-kit/ambient.d.ts
+++ apps/studio/.svelte-kit/ambient.d.ts
@@ -75,8 +75,8 @@ declare module '$env/static/private' {
 	export const XPC_FLAGS: string;
 	export const npm_config_node_gyp: string;
 	export const XPC_SERVICE_NAME: string;
-	export const pnpm_config_verify_deps_before_run: string;
 	export const npm_package_version: string;
+	export const pnpm_config_verify_deps_before_run: string;
 	export const VSCODE_INJECTION: string;
 	export const SHLVL: string;
 	export const HOME: string;
@@ -218,8 +218,8 @@ declare module '$env/dynamic/private' {
 		XPC_FLAGS: string;
 		npm_config_node_gyp: string;
 		XPC_SERVICE_NAME: string;
-		pnpm_config_verify_deps_before_run: string;
 		npm_package_version: string;
+		pnpm_config_verify_deps_before_run: string;
 		VSCODE_INJECTION: string;
 		SHLVL: string;
 		HOME: string;
